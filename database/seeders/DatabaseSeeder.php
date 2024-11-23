dc94981a7f94e188203a059efd8d3fff4dbb8337
wmcsantos <wilsoncavacosantos@gmail.com>
2024-11-23 22:52:05 +0000

    Project Boilerplate and Database Preparation (Project and Task Tables Created)

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Wilson',
+            'email' => 'wilson@example.com',
+            'password' => bcrypt('12345678'),
+            'email_verified_at' => time()
+        ]);
+
+        Project::factory()->count(30)->hasTasks(30)->create();
+    }
+}
