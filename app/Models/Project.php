e282d01179585041ffca098f25a19681631b3b6a
wmcsantos <wilsoncavacosantos@gmail.com>
2024-11-24 17:05:23 +0000

    Render Projects with Pagination

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -14,4 +14,14 @@ class Project extends Model
     {
         return $this->hasMany(Task::class);
     }
+
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updatedBy()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
 }
